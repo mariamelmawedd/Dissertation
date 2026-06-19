8edb589a0746385605c6f60fe1a8530b6561c4cf
mariamelmawedd <mariam.elmawedd@gmail.com>
2026-06-19 13:51:15 +0100

    Update EDA Diss_R code.R
    Finished basic EDA for the data

--- Diss_R code.R
+++ Diss_R code.R
@@ -24,17 +24,17 @@ list.files()
 glimpse(data_uk)
 data_uk %>% skim() #74901 rows
 
-############################
+########################################
+# Salary #
+##########
 
-#see if the Salary is related with the GPA
-ggplot(data_uk, aes(x=GPA, y=Salary))+geom_point()
-data_uk %>% get_correlation(GPA~ Salary) #very low 
-
+#Salary with numerical variables
 data_numerical<- data_uk %>% select(Salary, Years_Since_Graduation, GPA, Age)
 sum(is.na(data_numerical$Salary))
 sum(is.na(data_numerical$GPA))
 sum(is.na(data_numerical$Age))
 sum(is.na(data_numerical$Years_Since_Graduation))
+
 ggpairs(data_numerical)
 
 #use boxplots instead cause my variables are discrete numerical 
@@ -53,9 +53,13 @@ data_arrange<- data_uk %>%
   arrange(Years_Since_Graduation) %>%  filter(Employment_Status=="Continuing Education" & Years_Since_Graduation>=1)
 nrow(data_arrange) #is 17153 a big number? it is affecting the median tho, so we filter out or ignore?
 
+#summary for numerical
+summary(data_numerical)
+
+
 ###########################
 
-#Box plots with categorical variables
+#Salary with categorical variables
 
 library(gridExtra)
 p1<- ggplot(data_uk, aes(x=Internship_Experience, y=Salary))+
@@ -73,7 +77,7 @@ p6<- ggplot(data_uk, aes(x=Gender, y=Salary))+
 grid.arrange(p1, p2, p3, p4,p5,p6, ncol=2)
 
 #SALARY VS JOb_sector boxplot ]
-p6<- ggplot(data_uk, aes(x=Job_Sector, y=Salary))+
+ggplot(data_uk, aes(x=Job_Sector, y=Salary))+
   geom_boxplot()
 
 #salary vs country of origin 
@@ -82,8 +86,60 @@ p6<- ggplot(data_uk, aes(x=Job_Sector, y=Salary))+
 
 #boxplot for salary only
 boxplot(data_numerical$Salary)
-summary(data_numerical)
+
+
+#Slary vs Employment 
+ggplot(data_uk, aes(x=Employment_Status, y=Salary))+
+  geom_boxplot()
+
 
 
 
 #All i did today was lookign at salary and others, next look for employement status with other variables.
+
+
+########################################################################
+# Emplyemnet Status #
+#####################
+
+# Employment status distribution
+
+ggplot(data_uk,aes(x = Employment_Status, fill = Employment_Status)) +
+  geom_bar()
+
+
+# Employment by Field of Study
+
+ggplot(data_uk, aes(x = Field_of_Study, fill = Employment_Status)) +
+  geom_bar()
+
+
+# Employment by Visa Type
+ggplot(data_uk, aes(x = Visa_Type, fill = Employment_Status)) +
+  geom_bar()
+
+# Employment by Internship Experience
+ggplot(data_uk, aes(x = Internship_Experience, fill = Employment_Status)) +
+  geom_bar(position = "fill") # for proportion
+
+# Employment by University Ranking
+ggplot(data_uk, aes(x = University_Ranking, fill = Employment_Status)) +
+  geom_bar(position = "fill") # for proportion
+
+
+
+#Employement Class
+data_uk %>%
+  count(Employment_Status) %>%
+  mutate(prop = n / sum(n))
+
+
+
+###########################################################################
+
+#Maybe in the future the dataset can be cleaned to 
+emplyement_data <- data_uk %>%
+  filter((Employment_Status == "Employed" | Employment_Status=="Unemployed"))
+
+
+#basic EDA to explore Data. 
